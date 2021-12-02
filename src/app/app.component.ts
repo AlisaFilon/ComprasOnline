d6448ecc2106e13c09a2a5ea5de35f05908753d0
AlisaFilon <filonenkoalisa@gmail.com>
2021-12-03 00:22:22 +0100

    Trabajo con el Enrutamiento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ComprasOnline';
+   toolbarname :string ;
+
+
+  constructor() {
+   this.toolbarname='HOME';
+
+   }
+
+  ngOnInit() {
+
+  }
 }
